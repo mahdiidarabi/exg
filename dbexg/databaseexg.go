fe9953ddf51eac74f7e8d25ce49c80142366bd08
mahdiidarabi <mahdida97@gmail.com>
2020-10-10 15:59:52 +0330

    jdlgkdklsjg

--- dbexg/databaseexg.go
+++ dbexg/databaseexg.go
@@ -14,10 +14,10 @@ var DB *gorm.DB
 
 func SetConnection() (*gorm.DB, error) {
 
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("got error in loading .env")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	fmt.Println("got error in loading .env")
+	//}
 	//var dsn = fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable",
 	//	os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	//fmt.Println(dsn)
